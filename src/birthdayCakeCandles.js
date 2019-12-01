440915ba539a024c2edfb29be1659774e0ddbfa6
Oleksandr Sirchenko <oleksandr.sirchenko@gmail.com>
2019-12-01 19:30:20 +0200

    birthday cake candles

---
+++ src/birthdayCakeCandles.js
@@ -0,0 +1,16 @@
+function birthdayCakeCandles(ar) {
+  let itr = 1;
+  ar.sort((a, b) => {
+    return a - b;
+  }).reverse();
+
+  for (let candle in sortedArr) {
+    if (sortedArr[candle] < sortedArr[candle - 1]) {
+      return itr;
+    } else if (sortedArr[candle] === sortedArr[candle - 1]) {
+      itr++;
+    }
+  }
+
+  return itr;
+}

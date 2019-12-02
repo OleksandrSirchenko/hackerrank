9ab91282964435dc2687ec6c6eb2f79b0a44e1aa
Oleksandr Sirchenko <oleksandr.sirchenko@gmail.com>
2019-12-02 11:22:34 +0200

    grading students

---
+++ src/js/gradingStudents.js
@@ -0,0 +1,15 @@
+function gradingStudents(grades) {
+  function _roundToFive(x) {
+    return Math.ceil(x/5) * 5;
+  }
+
+  for (let grade in grades) {
+    let diff = _roundToFive(grades[grade]) - grades[grade];
+
+    if (diff < 3 && grades[grade] > 37) {
+      grades[grade] = _roundToFive(grades[grade]);
+    }
+  }
+
+  return grades;
+}

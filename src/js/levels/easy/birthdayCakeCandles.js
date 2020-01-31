69c094c10a5c4ee63e40e406f0a6c3db613f03e7
Alex <sirchenko.93@gmail.com>
2020-01-31 16:59:49 +0200

    Birthday cake candles fix

--- src/js/levels/easy/birthdayCakeCandles.js
+++ src/js/levels/easy/birthdayCakeCandles.js
@@ -1,14 +1,15 @@
 function birthdayCakeCandles(ar) {
     let itr = 1;
-      ar.sort((a, b) => {
-        return a - b;
-      }).reverse();
 
-    for (let candle in sortedArr) {
-        if (sortedArr[candle] < sortedArr[candle - 1]) {
-          return itr;
-        } else if (sortedArr[candle] === sortedArr[candle - 1]) {
-          itr++;
+    ar.sort((a, b) => {
+        return b - a;
+    });
+
+    for (let candle in ar) {
+        if (ar[candle] < ar[candle - 1]) {
+            return itr;
+        } else if (ar[candle] === ar[candle - 1]) {
+            itr++;
         }
     }
 

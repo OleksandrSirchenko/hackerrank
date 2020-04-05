231442b62f4f3b5b16cacdb157eeedd715e875e8
Oleksandr Sirchenko <alex-sirchenko@osirchenko-mbp.local>
2020-04-05 17:44:08 +0300

    Electronics Shop

---
+++ src/js/levels/easy/getMoneySpent.js
@@ -0,0 +1,13 @@
+const getMoneySpent = (keyboards, drives, b) => {
+    let max = -1;
+    
+    for (let i = 0; i < keyboards.length; i++) {
+        for (let j = 0; j < drives.length; j++) {
+            let sum = keyboards[i] + drives[j];
+            if (sum <= b && sum > max) {
+                max = sum;        
+            }
+        }
+    }
+    return max;
+}
